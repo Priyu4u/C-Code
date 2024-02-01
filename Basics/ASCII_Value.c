ace876a612a801e014c7395ce1903e7977c187bf
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-01 11:31:41 +0530

    C-basics

---
+++ Basics/ASCII_Value.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main( ){
+    char ch = 'a';
+    printf("%c\n",ch);
+    printf("%d\n",ch);
+    char chh = 'A';
+    printf("%c\n",chh);
+    printf("%d\n",chh);
+
+    // ASCII value of 'a' -> 97
+    // ASCII value of 'A' -> 65
+
+    char ch1 = '=';
+    printf("%c\n",ch1);
+    printf("%d\n",ch1);
+    char ch2 = '%';
+    printf("%c\n",ch2);
+    printf("%d\n",ch2);
+    char ch3 = '!';
+    printf("%c\n",ch3);
+    printf("%d\n",ch3);
+    char ch4 = '@';
+    printf("%c\n",ch4);
+    printf("%d\n",ch4);
+    char ch5 = '$';
+    printf("%c\n",ch5);
+    printf("%d\n",ch5);
+    char ch6 = '*';
+    printf("%c\n",ch6);
+    printf("%d\n",ch6);
+    char ch7 = '^';
+    printf("%c\n",ch7);
+    printf("%d\n",ch7);
+    char ch8 = '(';
+    printf("%c\n",ch8);
+    printf("%d\n",ch8);
+    char ch9 = '_';
+    printf("%c\n",ch9);
+    printf("%d\n",ch9);
+    char ch10 = '?';
+    printf("%c\n",ch10);
+    printf("%d\n",ch10);
+
+
+    return 0;
+}

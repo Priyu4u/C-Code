ca84794a85d6b9d4f3c62270c848b8ce04a81264
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-02 12:42:27 +0530

    realThingInIfElse

---
+++ if_else/theRealThing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main( ){
+    /* if( condition ){
+        code
+     } */ 
+
+
+    /* if( statement ){
+        code
+     } */
+
+    if (3 + 2 % 5){   // it is a statement not condition
+        printf("This works");
+    }
+    
+    return 0;
+}

7f89a63ed23b591754bef576845cf6dc11e2b60a
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-02 19:05:40 +0530

    loops

--- Loops/AP_47.c
+++ Loops/AP_47.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main( ){
-     int n;
+    // Display this AP 4 7 10 13 16 upto n terms
+    int n;
     printf("Enter the value of n : ");
     scanf("%d",&n);
-    for(int i=1; i<=2*n-1; i=i+2){
+    for(int i=4; i<=3*n+1; i=i+3){
         printf("%d ",i);
     }
     return 0;
---
+++ Loops/AP_withoutMaths.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main( ){
+    // Display this AP - 1, 3, 5, 7, 9.. upto n terms 
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    
+    return 0;
+}

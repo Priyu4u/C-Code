6c6c33ca7cee89f1e8b9f632cafad83b089d994a
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-03 16:12:58 +0530

    loops

---
+++ Loops/while-loop/predictOutput1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main( ){
+    // Predict the output
+    int j;          // Garbage value -> Random value can be assinged
+    printf("\n%d",j);
+    while (j<=10){
+        printf("\n%d",j);
+        j = j + 1;
+    }
+    
+    return 0;
+}
---
+++ Loops/while-loop/while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main( ){
+    // Print counting numbers from 1 to 100 using while loop
+    int i=1;  // initialisation before loop
+    while (i<=100){    // condition inside the loop
+        printf("%d ",i);
+        i++;   // increment inside the loop
+    }
+
+    // while loop is used when we do not have enough information
+    return 0;
+} 

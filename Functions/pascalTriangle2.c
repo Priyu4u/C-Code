c28aeb64a1b2dde7c14fb180d0a1dd63686cb830
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-10 21:24:01 +0530

    functions

---
+++ Functions/pascalTriangle2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main( ){
+    /*
+    Print the given pattern
+                                1
+                              1  1
+                            1  2   1
+                         1   3  3   1
+                      1   4   6   4   1
+                   1   5  10  10  5   1
+    */
+    int n;
+    printf("Enter number of rows : ");
+    scanf("%d",&n);
+
+    
+
+    return 0;
+}

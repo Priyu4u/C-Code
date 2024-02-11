171fa80944be3e836548587b1c30443a6a7e1e3a
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-11 20:57:11 +0530

    functions

--- Functions/pascalTriangle2.c
+++ Functions/pascalTriangle2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main( ){
     /*
-    Print the given pattern
+    Print the given pattern without using functions. Use maths
                                 1
                               1  1
                             1  2   1
@@ -9,11 +9,20 @@ int main( ){
                       1   4   6   4   1
                    1   5  10  10  5   1
     */
-    int n;
-    printf("Enter number of rows : ");
-    scanf("%d",&n);
+   int n;
+   printf("Enter number of rows : ");
+   scanf("%d",&n);
+    for(int i=0; i<=n-1; i++){
+         for(int k=1; k<=n-i; k++){
+            printf(" ");
+         }
+         int first = 1;
+         for(int j=0; j<=i; j++){
+            printf("%d ",first);
+            first = first * (i-j) / (j+1);
+         }
+      printf("\n");
+   }
 
-    
-
-    return 0;
+   return 0;
 }

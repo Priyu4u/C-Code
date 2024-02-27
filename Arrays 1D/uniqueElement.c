d6020d6d1fe7adb6f11ceab8a30b76b345a7aad8
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-02-27 23:38:50 +0530

    unique element

---
+++ Arrays 1D/uniqueElement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main( ){
+    // Find the unique number in a given array where all the elements are being repeated twice with one value being unique
+    int arr[7] = {1,3,4,2,1,2,3} ; 
+    for(int i=0; i<=6; i++){
+        bool flag = false ;
+        for(int j=i+1; j<=6; j++){
+            if(arr[i] == arr[j]){
+                flag = true ;
+            }
+        }
+        if(flag == false){
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+    return 0;
+}

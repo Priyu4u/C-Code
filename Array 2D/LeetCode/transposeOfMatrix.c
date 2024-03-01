0ff21aeaacf95909089f9bba1a848821685d49a9
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-01 23:07:19 +0530

    transpose of matrix

---
+++ Array 2D/LeetCode/transposeOfMatrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main( ){
+    // WAP to print the transpose of the matrix entered by the user and store it in separate matrix .(Leetcode - 867)
+    int r ,c ;
+    printf("Enter number of rows : ") ;
+    scanf("%d",&r) ;
+    printf("Enter number of columns : ") ;
+    scanf("%d",&c) ;
+    int arr[r][c] ;
+    printf("Enter elements of matrix : \n") ;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            scanf("%d",&arr[i][j]) ;
+        }
+    }
+    int arrTrans[c][r] ;
+    printf("Transpose of matrix : \n") ;
+    for(int i=0; i<c; i++){
+        for(int j=0; j<r; j++){
+            // printf("%d ",arr[j][i]) ;
+            arrTrans[i][j] = arr[j][i] ;
+        }
+    }
+    for(int i=0; i<c; i++){
+        for(int j=0; j<r; j++){
+            printf("%d ",arrTrans[i][j]) ;
+        }
+        printf("\n") ;
+    }
+    return 0;
+}

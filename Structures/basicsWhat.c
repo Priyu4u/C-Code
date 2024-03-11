bb58b8afe3a89f01e93d24327250c873e485ebd3
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-11 22:13:20 +0530

    basics of structures

---
+++ Structures/basicsWhat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main( ){
+    // Structures are user defined data types
+    struct pokemon{             // -> user defined data type
+        int hp;
+        int speed;
+        int attack;
+        char tier ;        // G S A B C D
+    } ;  // -> semicolon is mendatory in structures
+
+    struct pokemon pikachu ;
+    pikachu.hp = 50 ;
+    pikachu.speed = 100 ;
+    pikachu.attack = 70 ;
+    pikachu.tier = 'A' ;
+
+    struct pokemon charizard;
+    charizard.attack = 160 ;
+    charizard.hp = 80 ;
+    charizard.speed = 70 ;
+    charizard.tier = 'S' ;
+
+    struct pokemon mewtwo ;
+    mewtwo.attack = 180 ;
+    mewtwo.hp = 150 ;
+    mewtwo.speed = 120 ;
+    mewtwo.tier = 'G' ;
+
+    printf("%d\n",mewtwo.attack) ;
+    printf("%d\n",charizard.attack) ;
+    printf("%c\n",charizard.tier) ;
+    printf("%d\n",pikachu.hp) ; 
+
+    // we can also take input from user
+    printf("Enter attack of pikachu : ");
+    scanf("%d",&pikachu.attack) ;
+    // printing
+    printf("%d\n",pikachu.attack) ;  
+
+
+    return 0;
+}

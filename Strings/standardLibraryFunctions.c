c026a1fc415a5770879d0154d0e3f455382abab4
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-11 18:20:51 +0530

    some built in functions

--- Strings/standardLibraryFunctions.c
+++ Strings/standardLibraryFunctions.c
@@ -7,9 +7,32 @@ int main( ){
     printf("%d\n",length) ;
 
 
-    // strcpy(char *s1, char *s2) ->  copies the content of string1 to string2
+    // strcpy(char *s1, char *s2) ->  copies the content of string s2 to string s1 
+    char s1[ ] = "Priyu" ;
+    char s2[6] ;
+    strcpy(s2,s1) ;
+    s1[0] = 'p' ;                 //   -> deep copy 
+    printf("%s\n",s1) ;
+    printf("%s\n",s2) ; 
 
-    // strcat(char *s1, char *s2) -> concat s1 string with s2 and stores the result in s1
+    /*
+    char* s3 = "Priyanshu" ;     -> not valid
+    char* s4 ;
+    strcpy(*s4,*s3) ;
+    printf("%s\n",s3) ;
+    printf("%s\n",s4) ; 
+    */
+
+
+    // strcat(char *s1, char *s2) -> concat(add) s1 string with s2 and stores the result in s1
+    char s3[ ] = "Priyanshu " ;
+    char s4[ ] = "Gupta" ;
+    strcat(s3,s4) ; 
+    printf("%s\n",s3) ;
+    printf("%s\n",s4) ;
+
+ 
+
 
     // strcmp(char *s1, char *s2)  -> compares the two strings 
 

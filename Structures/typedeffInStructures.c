a9b7d9323c5743a4a241ffa8a788ee872ed9fbdc
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-12 16:07:06 +0530

    typedef in structures

---
+++ Structures/typedeffInStructures.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+// using typedef in structures
+int main( ){
+
+    typedef struct book{
+        char name[40] ;
+        float price ;
+        int noOfPages ;
+    } book ; 
+    
+    book doglapan ;         // -> valid
+    struct atomicHabits ;
+
+
+    return 0;
+}

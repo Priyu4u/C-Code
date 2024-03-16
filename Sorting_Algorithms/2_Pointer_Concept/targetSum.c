26caddf86f78d415c0ff89f8dc2a8900162a54d7
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-16 22:03:12 +0530

    target sum

--- Sorting_Algorithms/2_Pointer_Concept/targetSum.c
+++ Sorting_Algorithms/2_Pointer_Concept/targetSum.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 int main( ){
     // Question: Given an array of integers number that is sorted in non-decreasing order , find two numbers such that they add upto a specific target number 
+    int n ;
+    printf("Enter number of elements in array : ");
+    scanf("%d",&n) ;
+    int arr[n] ;
+    printf("Enter elements of array in sorted manner : \n");
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]) ; 
+    } 
+    int targetSum ;
+    printf("Enter target sum number : ");
+    scanf("%d",&targetSum) ;
+
+    int i = 0;
+    int j = n - 1;
+    while(i < j){
+        if(arr[i] + arr[j] == targetSum){
+            printf("%d %d\n",arr[i] , arr[j]) ;
+            break;
+        }
+        else if(arr[i] + arr[j] > targetSum){
+            j-- ;
+        }
+        if(arr[i] + arr[j] < targetSum){
+            i++;
+        }
+    }
+
 
 
     // What is sorting ?

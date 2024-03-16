217cfb56325c75837724ed9d4fcf9a9da90d6951
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-16 21:41:17 +0530

    duplicate element

--- Sorting_Algorithms/TimeAndSpaceComplexity/duplicateElement.c
+++ Sorting_Algorithms/TimeAndSpaceComplexity/duplicateElement.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 int main( ){
+    // given an array of integers with 1 to n elements and the size of the array is n + 1. One element is occuring more than once i.e. duplicate number is present . Find the duplicate element .
+    
+    // to solve this question we have 3 METHODS
+    /*
+    Analysis of time and space complexity
+    */
+
+    // METHOD 1 : 
+    // ALGORITHM
+    // loop inside a loop
+    // efficient -> in terms of space
+    // not efficient in terms of time
+
+
+
+
+
+    // METHOD 2 : 
+    // ALGORITHM
+    // using another array namely visited array
+    // efficient -> in terms of time
+    // not efficient -> in terms of space    because we are occupying extra space
+
+
+
+
+
+    // METHOD 3 : 
+    // ALGORITHM
+    // by using(sum of total elements of array - sum of elements from 1 to n )
+    // efficient in terms of time and space both
+
+
+
+
+
 
     return 0;
-}
+} 

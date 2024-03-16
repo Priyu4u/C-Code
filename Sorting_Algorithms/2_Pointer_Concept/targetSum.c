a4dccaa741d3ea77dff18e4f68706d15799aff1f
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-16 21:40:58 +0530

    target sum

---
+++ Sorting_Algorithms/2_Pointer_Concept/targetSum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main( ){
+    // Question: Given an array of integers number that is sorted in non-decreasing order , find two numbers such that they add upto a specific target number 
+
+
+    // What is sorting ?
+    // sorting an array means arranging it in ascending order 
+    // sort means ascending order
+    // sort in descending means arranging in descending order
+    return 0;
+}
